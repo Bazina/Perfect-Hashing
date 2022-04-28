7728c54b4edaceb14d2a5637c20d8b29fefb87fd
Mohamed Mamdouh <63814228+MohamedMamdouh18@users.noreply.github.com>
2022-04-28 15:51:16 +0200

    O(n^2) formula method

--- src/main/java/FormulaMethod/ON2.java
+++ src/main/java/FormulaMethod/ON2.java
@@ -1,7 +1,106 @@
-import java.util.LinkedList;
+package FormulaMethod;
 
-public class ChainingHashTable<K, V> {
-    class Entry {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ON2<Integer, V> {
+    private final int maxSize;
+    private final int prime;
+    public int no = 0;
+    private ArrayList<Entry<Integer, V>> entries;
+    private int size = 0, hashA, hashB;
+
+    public ON2(int maxSize) {
+        this.maxSize = (int) Math.pow(maxSize, 2);
+        this.entries = new ArrayList<>(this.maxSize);
+        initiate(this.maxSize);
+        prime = (int) Math.pow(2, 31) - 1;
+        randFactors();
+    }
+
+    private void randFactors() {
+        var rand = new Random();
+        hashA = Math.abs(rand.nextInt()) % prime;
+        hashB = (Math.abs(rand.nextInt()) + 1) % (prime - 1);
+    }
+
+    private int hashFunction(int key) {
+        return (int) ((((long) hashA * key) + hashB) % prime) % maxSize;
+    }
+
+    public void put(Integer key, V value) {
+        size++;
+        Entry<Integer, V> entry = new Entry<>(key, value);
+
+        int hash = hashFunction((int) key);
+
+        while (this.entries.get(hash) != null) {
+            reHash();
+            hash = hashFunction((int) key);
+        }
+        this.entries.set(hash, entry);
+    }
+
+    public V get(Integer key) {
+        int hash = hashFunction((int) key);
+        Entry<Integer, V> entry = this.entries.get(hash);
+        return (entry == null) ? null : entry.value;
+    }
+
+    public void remove(Integer key) {
+        if (!contains(key))
+            return;
+
+        int hash = hashFunction((int) key);
+        this.entries.set(hash, null);
+    }
+
+    private boolean contains(Integer key) {
+        return get(key) != null;
+    }
+
+    private void reHash() {
+        no++;
+
+        List<Entry<Integer, V>> data = new ArrayList<>(this.entries);
+        boolean reHashing = false;
+        while (!reHashing) {
+            this.entries = new ArrayList<>();
+            initiate(this.maxSize);
+            this.randFactors();
+
+            boolean flag = true;
+
+            for (Entry<Integer, V> datum : data) {
+                if (datum == null) continue;
+                int key = (int) datum.key;
+                int hash = hashFunction(key);
+                if (entries.get(hash) != null) {
+                    flag = false;
+                    break;
+                } else entries.set(hash, datum);
+            }
+
+            if (flag) reHashing = true;
+        }
+    }
+
+    private double loadFactor() {
+        return (size * 1.0) / (maxSize * 1.0);
+    }
+
+    public ArrayList<Entry<Integer, V>> getEntries() {
+        return entries;
+    }
+
+    private void initiate(int size) {
+        for (int i = 0; i < size; i++) {
+            this.entries.add(null);
+        }
+    }
+
+    static class Entry<K, V> {
         private final K key;
         private V value;
 
@@ -21,100 +120,5 @@ public class ChainingHashTable<K, V> {
         public void setValue(V value) {
             this.value = value;
         }
-    }
-
-    private LinkedList<Entry>[] entries;
-    private int maxSize, size = 0;
-    private boolean reHashing = false;
-
-    public ChainingHashTable(int maxSize) {
-        this.entries = new LinkedList[maxSize];
-        this.maxSize = maxSize;
-    }
-
-    public void put(K key, V value) {
-        Entry entry = getEntry(key);
-
-        if (entry != null) {
-            entry.value = value;
-            return;
-        }
-
-        size++;
-        LinkedList<Entry> bucket = getOrCreateBucket(key);
-        if (bucket != null)
-            bucket.addLast(new Entry(key, value));
-
-        if (!this.reHashing && loadFactor() > 0.75)
-            reHash();
-    }
-
-    public V get(K key) {
-        Entry entry = getEntry(key);
-
-        return (entry == null) ? null : entry.value;
-    }
-
-    public void remove(K key) {
-        Entry entry = getEntry(key);
-
-        if (entry == null)
-            throw new IllegalStateException();
-
-        getBucket(key).remove(entry);
-        size--;
-
-        if (!this.reHashing && loadFactor() > 0.75)
-            reHash();
-    }
-
-    private LinkedList<Entry> getOrCreateBucket(K key) {
-        int index = hash(key);
-
-        if (entries[index] == null)
-            entries[index] = new LinkedList<>();
-
-        return entries[index];
-    }
-
-    private Entry getEntry(K key) {
-        LinkedList<Entry> bucket = getBucket(key);
-
-        if (bucket != null)
-            for (Entry entry : bucket)
-                if (entry.key == key)
-                    return entry;
-
-        return null;
-    }
-
-    private LinkedList<Entry> getBucket(K key) {
-        return entries[hash(key)];
-    }
-
-    private int hash(K key) {
-        return key.hashCode() % entries.length;
-    }
-
-    private void reHash() {
-        LinkedList<Entry>[] entriesCopy = this.entries.clone();
-
-        this.reHashing = true;
-        this.entries = new LinkedList[maxSize * 2];
-
-        for (int i = 0; i < maxSize; i++)
-            if (entriesCopy[i] != null)
-                for (Entry entry : entriesCopy[i])
-                    put(entry.key, entry.value);
-
-        this.maxSize = maxSize * 2;
-    }
-
-    public double loadFactor() {
-        return (size * 1.0) / (maxSize * 1.0);
-    }
-
-    public LinkedList<Entry>[] getEntries() {
-        return entries;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,22 @@
+import FormulaMethod.ON2;
 
 public class Main {
     public static void main(String[] args) {
-        String x = Integer.toBinaryString(-15);
-        String[] h = ;
+        ON2<Integer , Integer> x = new ON2<>(10) ;
+//        for (int i = 1; i < 51; i++) {
+//            System.out.println("x.put(" + i + "," + i + ");");
+//        }
+        x.put(1,1);
+        x.put(2,2);
+        x.put(3,3);
+        x.put(4,4);
+        x.put(5,5);
+        x.put(6,6);
+        x.put(7,7);
+        x.put(8,8);
+        x.put(9,9);
+        x.put(10,10);
+
+        System.out.println(x.no);
     }
 }
